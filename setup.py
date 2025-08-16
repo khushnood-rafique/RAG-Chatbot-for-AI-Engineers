a92addf0102588fe064994ee741e8bfcdd142d18
khushnood-rafique <khushnood.rafique@cs.uni-kl.de>
2025-08-16 16:56:46 +0200

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rag_chatbot_for_ai_engineers",
+    version="0.1.0",
+    author="Khushnood Adil Rafique",
+    author_email="khushnood.rafique@cs.uni-kl.de",
+    packages = find_packages(),
+    install_requires=[]
+)
