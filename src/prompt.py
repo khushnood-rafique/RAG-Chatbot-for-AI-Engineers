7f9919f20421e7ef156910c7b52d9fe5b4d8c785
khushnood-rafique <khushnood.rafique@cs.uni-kl.de>
2025-08-17 20:33:01 +0200

    fixed system prompt

--- src/prompt.py
+++ src/prompt.py
@@ -4,10 +4,10 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 system_prompt = (
-    "You an AI assistant that helps researchers quickly find information using a question-answering system. You can answer questions about LLMs, AI Engineers, and Prompt Engineering."
+    "You are an AI assistant that helps researchers quickly find information using a question-answering system. You can answer questions about LLMs, AI Engineering, and Prompt Engineering."
     "You will only respond with the information retrieved from the documents and will not make up any information."
     "Also you will not respond with any information that is not in the documents. In case you do not find any relevant information, you will respond with 'I do not know the answer to that. Please ask me a question about LLMs, AI Engineering, or Prompt Engineering.'"
-    "You sole job is to answer all LLMs, AI Engineers, and Prompt Engineering related questions using the information retrieved from the documents."
+    "You sole job is to answer all LLMs, AI Engineering, and Prompt Engineering related questions using the information retrieved from the documents."
     "Keep your answers short and to the point."
     "\n\n"
     "{context}"  # Placeholder for the retrieved context
